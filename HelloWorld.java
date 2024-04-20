e634b699fc1821d690623e768e5beecd323a30de
Pushick <167629017+Pushick@users.noreply.github.com>
2024-04-20 23:32:23 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("카운트를 시작합니다.");
+        for (int i = 10; i >= 0 ; i--){
+            System.out.println(i+"!!");
+
+        }
+        System.out.println("발사!");
+    }
+}
